9f515c16e711589534058c8916445504b831dc49
jonsoku2 <whdtjr2792@gmail.com>
2021-02-13 00:29:11 +0900

    hook 과 next session 을 사용한 로그인기능

--- pages/logout.tsx
+++ pages/logout.tsx
@@ -1,10 +1,27 @@
 import { GetServerSideProps, NextPage } from 'next';
+import { useEffect } from 'react';
 
+import { UserProps } from '../shared/apis';
+import useUser from '../shared/hooks/useUser';
 import withSession from '../shared/session';
 
-interface Props {}
+interface Props {
+  initialUser: UserProps;
+}
 
-const LogoutPage: NextPage<Props> = () => {
+const LogoutPage: NextPage<Props> = ({ initialUser }) => {
+  const { user, logout } = useUser({
+    redirectTo: '/login',
+    redirectIfFound: true,
+    initialUser
+  });
+
+  useEffect(() => {
+    if (user.isLoggedIn) {
+      logout();
+    }
+  }, [user, logout]);
+
   return <div>LogoutPage</div>;
 };
 
@@ -18,15 +35,13 @@ export const getServerSideProps: GetServerSideProps<Props> = withSession(async (
         destination: '/login'
       }
     };
-  } else {
-    req.session.destroy();
-    return {
-      redirect: {
-        permanent: false,
-        destination: '/login'
-      }
-    };
   }
+
+  return {
+    props: {
+      initialUser
+    }
+  };
 });
 
 export default LogoutPage;
